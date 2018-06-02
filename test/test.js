a31dac4f076683a367f3b0f5b91beefd0d092873
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-06-03 00:18:14 +0100

    coverage.. minor test changes

--- test/test.js
+++ test/test.js
@@ -8,27 +8,15 @@ const runner = new TestRunner()
 
 runner.test('loadModule: unknown path', function () {
   a.throws(
-    () => {
-      const mod = loadModule('./adsfdf')
-      console.error(require('util').inspect(mod, { depth: 6, colors: true }))
-    },
-    err => {
-      return err.code === 'MODULE_NOT_FOUND'
-    }
+    () => loadModule('./adsfdf'),
+    err => err.code === 'MODULE_NOT_FOUND'
   )
 })
 
 runner.test('loadModule: unknown path, module-dir', function () {
   a.throws(
-    () => {
-      const mod = loadModule('./adsfdf', {
-        moduleDir: '/some/where/wrong'
-      })
-      console.error(require('util').inspect(mod, { depth: 6, colors: true }))
-    },
-    err => {
-      return err.code === 'MODULE_NOT_FOUND'
-    }
+    () => loadModule('./adsfdf', { moduleDir: '/some/where/wrong' }),
+    err => err.code === 'MODULE_NOT_FOUND'
   )
 })
 
@@ -38,7 +26,7 @@ runner.test('loadModule: absolute path to lib', function () {
   a.strictEqual(module.name, 'arrayify')
 })
 
-runner.test('loadModule: relative path to a dir', function () {
+runner.test('loadModule: absolute path to a dir', function () {
   const modulePath = path.resolve(__dirname, 'fixture/loadModule/some-module')
   const module = loadModule(modulePath)
   a.strictEqual(module.name, 'someModule')
@@ -55,16 +43,16 @@ runner.test('loadModule: partial module name (module-prefix supplied)', function
 })
 
 runner.test('loadModule: full module name (module-prefix supplied)', function () {
-  const module = loadModule('back', { modulePrefix: 'array-' })
+  const module = loadModule('array-back', { modulePrefix: 'array-' })
   a.strictEqual(module.name, 'arrayify')
 })
 
-runner.test('loadModule: full module name, current dir default', function () {
+runner.test('loadModule: relative path to module dir, current dir default', function () {
   const module = loadModule('test/fixture/loadModule/some-module')
   a.strictEqual(module.name, 'someModule')
 })
 
-runner.test('loadModule: full module name, current dir default, module-dir', function () {
+runner.test('loadModule: relative path to module dir, current dir default, moduleDir', function () {
   const module = loadModule('test/fixture/loadModule/some-module', {
     moduleDir: '/some/where'
   })
